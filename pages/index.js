c54e853fd63c4745976e0c8102ad3f5720f695e4
Ramón Huidobro <hola@ramonh.dev>
2022-06-07 16:19:10 +0200

    Use netlify function

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { useState } from 'react/cjs/react.development'
 
 export default function Home() {
+  const [hello, setHello] = useState("");
+
+  fetch('./.netlify/functions/hello').then(data => data.json()).then(data => setHello(data.message))
   return (
     <div className="container">
       <Head>
@@ -15,6 +19,8 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+
+        <p>Message from Netlify: {hello}</p>
       </main>
 
       <Footer />
